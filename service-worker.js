c82423896c47e1c0dac7338ef9a00782fd78bc69
nishcthulhu <nishthedevil@gmail.com>
2020-04-26 14:25:40 +0000

    Deploying to gh-pages from  @ 3bd36feb6eecc7e53c94060c71aa0a5e494784a4 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.975670d682bced2df383a7c655ce04b9.js"
+  "/precache-manifest.059d79fef27fe2039268f9be908b0c88.js"
 );
 
 self.addEventListener('message', (event) => {
