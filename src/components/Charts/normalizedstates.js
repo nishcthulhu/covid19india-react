79c8b45bad727b9c7b2d9b7a57c567ad8ce506f2
Poirot <nishthedevil@gmail.com>
2020-04-26 19:51:29 +0530

    normalizedplots

--- src/components/Charts/normalizedstates.js
+++ src/components/Charts/normalizedstates.js
@@ -4,40 +4,43 @@ import {useEffectOnce, useLocalStorage, useFavicon} from 'react-use';
 import * as d3 from 'd3';
 import moment from 'moment';
 
-import {sliceTimeseriesFromEnd, formatNumber} from '../../utils/commonfunctions';
+import {
+  sliceTimeseriesFromEnd,
+  formatNumber,
+} from '../../utils/commonfunctions';
 import {useResizeObserver} from '../../utils/hooks';
 
 const accumulateData = (data) => {
-	let cumulativeData = [];
-	let temp = [];
-	let slice = [];
-	let value = 0;
-	let i = 0;
-	
-	cumulativeData = data.map((obj, index, arr) => {
-			temp[index] = {};
-			Object.keys(obj).forEach((key) => {
-				if (key === 'status') {
-					return
-				}
-				if (key === 'date') {
-					value = moment(obj[key], "DD-MMM-YY");
-				} else {
-					i = index;
-					let sum = 0;
-					while (i >= 0) {
-						sum += (parseInt(arr[i][key]) || 0)
-						i--;
-					}
-					value = parseInt(sum) || 0;
-				}
-				temp[index] = { ...temp[index], [key]: value };
-			})
-		return temp[index];
-	})
-	return cumulativeData;
+  let cumulativeData = [];
+  let temp = [];
+  let slice = [];
+  let value = 0;
+  let i = 0;
 
-	/*let cumulativeData = {};
+  cumulativeData = data.map((obj, index, arr) => {
+    temp[index] = {};
+    Object.keys(obj).forEach((key) => {
+      if (key === 'status') {
+        return;
+      }
+      if (key === 'date') {
+        value = moment(obj[key], 'DD-MMM-YY');
+      } else {
+        i = index;
+        let sum = 0;
+        while (i >= 0) {
+          sum += parseInt(arr[i][key]) || 0;
+          i--;
+        }
+        value = parseInt(sum) || 0;
+      }
+      temp[index] = {...temp[index], [key]: value};
+    });
+    return temp[index];
+  });
+  return cumulativeData;
+
+  /*let cumulativeData = {};
 	data.forEach((fD, index) => {
 		Object.keys(fD).forEach((key) => {
 			if (cumulativeData[key] === undefined) {
@@ -55,151 +58,151 @@ const accumulateData = (data) => {
 		})
 	})
 	return cumulativeData;*/
-}
+};
 
 const smoothened = (data, N) => {
-	let cumulativeData = [];
-	let temp = [];
-	let slice = [];
-	let value = 0;
-	let i = 0;
-	
-	cumulativeData = data.map((obj, index, arr) => {
-		slice = arr.slice(index, index + N);
-		if (index < arr.length - N + 1) {
-			temp[index] = {};
-			Object.keys(obj).forEach((key) => {
-				if (key === 'status') {
-					return
-				}
-				if (key === 'date') {
-					value = moment(obj[key], "DD-MMM-YY");
-				} else {
-					i = N-1;
-					let sum = 0;
-					while (i >= 0) {
-						sum += (parseInt(slice[i][key]) || 0)
-						i--;
-					}
-					value = parseInt(sum / N) || 0;
-				}
-				temp[index] = { ...temp[index], [key]: value };
-			})
-			return temp[index];
-		} else {
-			return;
-		}
-		return;
-	})
-	return cumulativeData;
-}
+  let cumulativeData = [];
+  let temp = [];
+  let slice = [];
+  let value = 0;
+  let i = 0;
+
+  cumulativeData = data.map((obj, index, arr) => {
+    slice = arr.slice(index, index + N);
+    if (index < arr.length - N + 1) {
+      temp[index] = {};
+      Object.keys(obj).forEach((key) => {
+        if (key === 'status') {
+          return;
+        }
+        if (key === 'date') {
+          value = moment(obj[key], 'DD-MMM-YY');
+        } else {
+          i = N - 1;
+          let sum = 0;
+          while (i >= 0) {
+            sum += parseInt(slice[i][key]) || 0;
+            i--;
+          }
+          value = parseInt(sum / N) || 0;
+        }
+        temp[index] = {...temp[index], [key]: value};
+      });
+      return temp[index];
+    } else {
+      return;
+    }
+    return;
+  });
+  return cumulativeData;
+};
 
 const repositioned = (data, threshold) => {
-	let cumulativeData = {};
-	Object.keys(data).forEach((key)=>{
-		(data[key]).forEach((value,index)=>{
-			if (cumulativeData[key] === undefined) {
-				cumulativeData[key] = [] ;
-  			}
-			if (data[key][index] >= threshold ){				
-				cumulativeData[key] = [...cumulativeData[key], data[key][index]];
-			}
-		})
-	})
-	return cumulativeData;
-}
-
+  let cumulativeData = {};
+  Object.keys(data).forEach((key) => {
+    data[key].forEach((value, index) => {
+      if (cumulativeData[key] === undefined) {
+        cumulativeData[key] = [];
+      }
+      if (data[key][index] >= threshold) {
+        cumulativeData[key] = [...cumulativeData[key], data[key][index]];
+      }
+    });
+  });
+  return cumulativeData;
+};
 
 function NormalizedStates(props) {
-	
-	const filterData = props.data.filter((object) => {
-				return object.status === 'Confirmed';
-			}).sort((a, b) => {
-				return moment(a.date, "DD-MMM=YY") - moment(b.date, "DD-MMM=YY")
-			});
-	const [cumulativeData, dates] = useMemo (
-	    () => {
-			//const cumulativeData = repositioned (smoothened((accumulateData(filterData)),3),10);	
-			//const cumulativeData = smoothened((accumulateData(filterData)),3);	
-			const dates = filterData.map(x => moment(x.date, "DD-MMM-YY"));
-			//console.log (cumulativeData);		
-			return [[], dates]
-	    },
-	    [filterData]
-  	);		
-	const temp = accumulateData(smoothened(filterData,1).filter((obj) => {return obj !== undefined}));
+  const filterData = props.data
+    .filter((object) => {
+      return object.status === 'Confirmed';
+    })
+    .sort((a, b) => {
+      return moment(a.date, 'DD-MMM=YY') - moment(b.date, 'DD-MMM=YY');
+    });
+  const [cumulativeData, dates] = useMemo(() => {
+    //const cumulativeData = repositioned (smoothened((accumulateData(filterData)),3),10);
+    //const cumulativeData = smoothened((accumulateData(filterData)),3);
+    const dates = filterData.map((x) => moment(x.date, 'DD-MMM-YY'));
+    //console.log (cumulativeData);
+    return [[], dates];
+  }, [filterData]);
+  const temp = accumulateData(
+    smoothened(filterData, 1).filter((obj) => {
+      return obj !== undefined;
+    })
+  );
 
-	const graphData = useCallback (()=>{
-		// set the dimensions and margins of the graph
-		var margin = {top: 10, right: 30, bottom: 30, left: 60},
-			width = 1000 - margin.left - margin.right,
-			height = 400 - margin.top - margin.bottom;
-		const keys = Object.keys(temp[0]);	
-		const hues = new Map(keys.map(d => 
-			[d, Math.floor(
-				(Math.random() * 360)
-				)
-			])
-		);
-		console.log(temp);
-		const xScale = d3.scaleTime()
-			.domain(d3.extent(dates))
-			.range([0,width]);
-			
-		const yScale = d3.scaleLog()
-			.clamp(true)
-			.domain([50, 28000])
-			.rangeRound([height, 0]);
+  const graphData = useCallback(() => {
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+      width = 1000 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+    const keys = Object.keys(temp[0]);
+    const hues = new Map(keys.map((d) => [d, Math.floor(Math.random() * 360)]));
+    console.log(temp);
+    const xScale = d3.scaleTime().domain(d3.extent(dates)).range([0, width]);
 
-		const yaxis = d3.axisLeft().scale(yScale); 
-		const xaxis = d3.axisBottom().scale(xScale);
+    const yScale = d3
+      .scaleLog()
+      .clamp(true)
+      .domain([50, 28000])
+      .rangeRound([height, 0]);
 
-		var svg = d3.select("#chart")
-			.append("svg")
-				.attr("width", width +  margin.left + margin.right)
-				.attr("height", height +  margin.top + margin.bottom)
-				.attr("fill", "red")
-				.style("color", "blue")
+    const yaxis = d3.axisLeft().scale(yScale);
+    const xaxis = d3.axisBottom().scale(xScale);
 
-			.style("color", "blue")
-			.append("g")
-				.attr("transform",
-					"translate(" + margin.left +","+margin.top+")");
+    var svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('fill', 'red')
+      .style('color', 'blue')
 
-		svg.append("g")
-		    .attr("class", "axis")
-		    .attr("transform", "translate(0," + height + ")")
-		    .call(xaxis);
-		   
-		svg.append("g")
-		    .attr("class", "axis")
-		    .call(yaxis);
-	 
-	    keys.forEach ((key,index)=>{
-	    	if (key === 'date' || key === 'status') {
-				return;
-			} else {
-				svg.append("path")
-				.datum(temp)
-				.attr("fill", "none")
-				.attr("stroke", `hsl(${hues.get(key)}, 70%, 30%)`)
-				.attr("stroke-width", 1.5)
-				.attr("d", d3.line()
-			        .x(function(d) { return xScale(moment(d.date))})
-			        .y(function(d) { return yScale(parseInt(d[key]))
-			        }))
+      .style('color', 'blue')
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-			};
-	    })
-	}, [temp, dates]);
+    svg
+      .append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xaxis);
 
+    svg.append('g').attr('class', 'axis').call(yaxis);
 
-	useEffect(() => {
-		graphData();
-	}, [graphData]);
+    keys.forEach((key, index) => {
+      if (key === 'date' || key === 'status') {
+        return;
+      } else {
+        svg
+          .append('path')
+          .datum(temp)
+          .attr('fill', 'none')
+          .attr('stroke', `hsl(${hues.get(key)}, 70%, 30%)`)
+          .attr('stroke-width', 1.5)
+          .attr(
+            'd',
+            d3
+              .line()
+              .x(function (d) {
+                return xScale(moment(d.date));
+              })
+              .y(function (d) {
+                return yScale(parseInt(d[key]));
+              })
+          );
+      }
+    });
+  }, [temp, dates]);
 
-	    // Add X axis --> it is a date format
-    /*var x = d3.scaleTime()
+  useEffect(() => {
+    graphData();
+  }, [graphData]);
+
+  // Add X axis --> it is a date format
+  /*var x = d3.scaleTime()
       .domain(d3.extent(data, function(d) { return d.date; }))
       .range([ 0, width ]);
     svg.append("g")
@@ -222,11 +225,8 @@ function NormalizedStates(props) {
         .x(function(d) { return x(d.index) })
         .y(function(d) { return y(d[index]) })
         );*/
-	
 
-
-
-/*	useEffectOnce(()=>{
+  /*	useEffectOnce(()=>{
 
 	const svg = useRef();
 	const width = 512;
@@ -346,16 +346,12 @@ function NormalizedStates(props) {
 	}, cumulativeData)
 
 */
-	
 
-
-	return (
-	    <React.Fragment>
-
-			<div id = "chart">
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div id="chart"></div>
+    </React.Fragment>
+  );
 }
 
 export default React.memo(NormalizedStates);
